4c5c4e47e3be49c636dc3402f29f81d4adb52224
JuanRuiz2406 <44301111+JuanRuiz2406@users.noreply.github.com>
2021-03-25 12:11:54 -0600

    Formulario Login
    Incluye manejo de errores:
    
    - Correo Electrónico Required,
    con Formato Válido
    
    - Contraseña Required,
    con mínimo 8 caracteres

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,9 +1,17 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../auth/AuthContext";
 import { types } from "../../types/types";
+import { useForm } from "react-hook-form";
 
 const Login = ({ history }) => {
   const { dispatch } = useContext(AuthContext);
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    handleLogin();
+  };
+  console.log(errors);
 
   const handleLogin = () => {
     const lastPath = localStorage.getItem("lastPath") || "/";
@@ -23,7 +31,47 @@ const Login = ({ history }) => {
     <div>
       <h1>Login</h1>
 
-      <button onClick={handleLogin}>Iniciar Sesión</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h3>Correo Electrónico</h3>
+        <input
+          type="text"
+          placeholder="Email"
+          name="Email"
+          ref={register({
+            required: {
+              value: true,
+              message: "*El Correo Electrónico es obligatorio*",
+            },
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "*El Correo Electrónico debe tener un formato válido*",
+            },
+          })}
+        />
+        <span>{errors?.Email?.message}</span>
+
+        <h3>Contraseña</h3>
+        <input
+          type="password"
+          placeholder="Contraseña"
+          name="Password"
+          ref={register({
+            required: {
+              value: true,
+              message: "*La Contraseña es obligatoria*",
+            },
+            minLength: {
+              value: 8,
+              message: "*La Contraseña debe tener mínimo 8 caracteres*",
+            },
+          })}
+        />
+        <span>{errors?.Password?.message}</span>
+
+        <input type="submit" />
+      </form>
+
+      {/* <button onClick={handleLogin}>Iniciar Sesión</button> */}
     </div>
   );
 };
