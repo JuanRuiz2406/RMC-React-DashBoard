1f480d1b132bd01d6f1b1bae9d17a6b267186635
dvperga <44186637+dvperga@users.noreply.github.com>
2021-05-21 04:21:01 -0600

    navbar

--- src/components/ui/navbar.js
+++ src/components/ui/navbar.js
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { useHistory } from "react-router";
 import { AuthContext } from "../../auth/AuthContext";
 import { types } from "../../types/types";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import logo from "../../images/ReportsMyCity.png";
+import person from "../../images/avatar.png";
 
 import { Link } from "react-router-dom";
 
@@ -20,15 +23,26 @@ export const NavBar = () => {
   };
 
   return (
-    <div className="container">
-      Navbar
-      <Link to="/">Reportes</Link>
-      {user.role === "RMCTeam" ? (
-        <Link to="/municipalidades">Municipalidades</Link>
-      ) : (
-        <Link to="/municipalidades">Ver Municipalidad</Link>
-      )}
-      <button onClick={handleLogout}>Cerrar Sesión</button>
+    <div className="container-fluid">
+      <nav className="navbar navbar-light bg-white">
+        <Link className="navbar-brand" to="/user">
+          <img className="rounded rounded-circle" src={person} style={{ width: "70px", height: "70px", margin: '0% 10%' }} />
+          <Link className="navbar-brand" to="/">Reportes</Link>
+          {user.role === "RMCTeam" ? (
+            <Link className="navbar-brand" to="/municipalidades">Municipalidades</Link>
+          ) : (
+            <Link className="navbar-brand" to="/municipalidades">Ver Municipalidad</Link>
+          )}
+
+
+        </Link>
+
+        <span className="navbar-text">
+          <img src={logo} style={{ width: "300px", height: "70px" }} />
+        </span>
+
+
+      </nav>
     </div>
   );
 };
