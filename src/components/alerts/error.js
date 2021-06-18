da2da039e51cb066c4508b615be453ff9d945a31
JuanRuiz2406 <44301111+JuanRuiz2406@users.noreply.github.com>
2021-06-17 22:19:50 -0600

    Color de botón de Alert Error

--- src/components/alerts/error.js
+++ src/components/alerts/error.js
@@ -1,7 +1,12 @@
 import Swal from "sweetalert2";
 
 const Error = (text) => {
-  Swal.fire("Error", text, "error");
+  Swal.fire({
+    title: "Error",
+    text: text,
+    icon: "error",
+    confirmButtonColor: "#3085d6",
+  });
 };
 
 export default Error;
