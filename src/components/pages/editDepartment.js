978eb7def2c91078af14393da0c331481b04f606
dvperga <diegoskate98@gmail.com>
2021-06-20 21:56:22 -0600

    Formulario de edicion departamento
    Listo

--- src/components/pages/editDepartment.js
+++ src/components/pages/editDepartment.js
@@ -2,7 +2,17 @@ import React from "react";
 import { useHistory } from "react-router";
 import { useForm } from "react-hook-form";
 import { updateDepartment } from "../../services/departments";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  makeStyles,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import { Success, Error } from "../alerts";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 const EditDepartment = () => {
   const history = useHistory();
@@ -40,88 +50,137 @@ const EditDepartment = () => {
       Error(updateResponse.error);
     }
   };
-
+  const classes = useStyles();
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h1>Editar Departamento</h1>
-
-        <h3>Nombre</h3>
-        <input
-          type="text"
-          placeholder="Nombre del Departamento"
-          defaultValue={departmentData.name}
-          name="name"
-          ref={register({
-            required: {
-              value: true,
-              message: "*El Nombre es obligatorio*",
-            },
-          })}
-        />
-        <span>{errors?.name?.message}</span>
-
-        <h3>Descripción</h3>
-        <input
-          type="text"
-          placeholder="Descripción del Departamento"
-          defaultValue={departmentData.description}
-          name="description"
-          ref={register({
-            required: {
-              value: true,
-              message: "*La descripción es obligatoria*",
-            },
-          })}
-        />
-        <span>{errors?.description?.message}</span>
-
-        <h3>Correo Electrónico</h3>
-        <input
-          type="text"
-          placeholder="ejemplo@departamento.com"
-          defaultValue={departmentData.email}
-          name="email"
-          ref={register({
-            required: {
-              value: true,
-              message: "*El Correo Electrónico es obligatorio*",
-            },
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "*El Correo Electrónico debe tener un formato válido*",
-            },
-          })}
-        />
-        <span>{errors?.email?.message}</span>
-
-        <h3>Teléfono</h3>
-        <input
-          type="text"
-          placeholder="Número de teléfono"
-          defaultValue={departmentData.telephone}
-          name="telephone"
-          ref={register({
-            required: {
-              value: true,
-              message: "*El teléfono es obligatorio*",
-            },
-          })}
-        />
-        <span>{errors?.telephone?.message}</span>
-
-        <input type="submit" value="Editar" />
-
-        <button
+    <Box bgcolor="background.default" p={2}>
+      <Container>
+        <Button
+          style={{ marginTop: 30 }}
+          color="primary"
           onClick={() => {
-            history.push("/municipalidades/departamentos");
+            history.goBack();
           }}
         >
-          Volver
-        </button>
-      </form>
-    </div>
+          <ArrowBackIcon style={{ color: "#0277BD", fontSize: 40 }} /> Volver
+        </Button>
+
+        <Grid container spacing={4} style={{ marginTop: 10 }}>
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+            <Box bgcolor="common.white" p={1.5} boxShadow={2}>
+              <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+                <Typography variant="h5" gutterBottom>
+                  Editar Departamento
+                </Typography>
+                <TextField
+                  id="standard-basic"
+                  margin="normal"
+                  fullWidth
+                  label="Nombre del Departamento"
+                  type="text"
+                  name="name"
+                  defaultValue={departmentData.name}
+                  inputRef={register({
+                    required: "El nombre es requerido.",
+                  })}
+                  error={Boolean(errors.name)}
+                  helperText={errors.name?.message}
+                />
+
+                <TextField
+                  id="standard-basic"
+                  margin="normal"
+                  fullWidth
+                  label="Descripcion"
+                  type="text"
+                  name="description"
+                  defaultValue={departmentData.description}
+                  inputRef={register({
+                    required: "El apellido es requerido.",
+                  })}
+                  error={Boolean(errors.description)}
+                  helperText={errors.description?.message}
+                />
+
+                <TextField
+                  id="standard-basic"
+                  margin="normal"
+                  fullWidth
+                  label="Correo del encargado"
+                  type="email"
+                  name="email"
+                  defaultValue={departmentData.email}
+                  inputRef={register({
+                    required: "El correo es requerido.",
+                  })}
+                  error={Boolean(errors.email)}
+                  helperText={errors.email?.message}
+                />
+
+                <TextField
+                  id="standard-basic"
+                  margin="normal"
+                  fullWidth
+                  label="Numero de telefono"
+                  type="number"
+                  name="telephone"
+                  defaultValue={departmentData.telephone}
+                  inputRef={register({
+                    required: "La tarjeta de identificaion es requerida.",
+                  })}
+                  error={Boolean(errors.telephone)}
+                  helperText={errors.telephone?.message}
+                />
+
+                <Button
+                  type="submit"
+                  className={classes.submit}
+                  fullWidth
+                  style={{ marginTop: "10%", background: "#4caf50" }}
+                  variant="contained"
+                >
+                  Editar
+                </Button>
+              </form>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
 export default EditDepartment;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 300,
+    },
+    textField: {
+      width: "25ch",
+    },
+    button: {
+      margin: theme.spacing(1),
+    },
+  },
+  form: {
+    marginLeft: "10%",
+    marginRight: "10%",
+    width: "80%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 5),
+  },
+  forgot: {
+    color: theme.palette.primary.main,
+    marginTop: theme.spacing(8),
+  },
+  title: {
+    color: theme.palette.text.secondary,
+  },
+  next: {
+    margin: theme.spacing(3, 0, 5),
+  },
+}));
