8dd271919932bcf3a416dc830af88bec209a4116
dvperga <diegoskate98@gmail.com>
2021-07-01 23:53:04 -0600

    Para deploy

--- src/components/ui/map.js
+++ src/components/ui/map.js
@@ -10,7 +10,7 @@ const Map = () => {
 
   const { ref, map, google } = useGoogleMaps(
     // Use your own API key, you can get one from Google (https://console.cloud.google.com/google/maps-apis/overview)
-    "AIzaSyBEuDt_qDnSmmFuTN0i2MX3OCcx9Ps9Blo",
+    "AIzaSyC3otWzHIFJrFnswjevge5wgPwfBC5M_kY",
     // NOTE: even if you change options later
     {
       center: {
@@ -28,7 +28,7 @@ const Map = () => {
 
   console.log(map); // instance of created Map object (https://developers.google.com/maps/documentation/javascript/reference/map)
   console.log(google); // google API object (easily get google.maps.LatLng or google.maps.Marker or any other Google Maps class)
-  return <div ref={ref} style={{ width: '100%', height: 200 }} />;
+  return <div ref={ref} style={{ width: "100%", height: 200 }} />;
 };
 
 export default Map;
